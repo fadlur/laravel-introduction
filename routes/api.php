3c8c6391d50016df855a6f45c6d338c87889a100
Fadlur <fadloer@gmail.com>
2022-08-06 20:30:53 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+// Route::get('sensor', function () {
+//     $data = [
+//         'status' => true,
+//         'message' => 'Data sensor ditemukan',
+//         'data' => null,
+//         'errors' => null,
+//     ];
+//     return response()->json($data, 200);
+// });
+// Route::post('sensor', function () {
+//     $data = [
+//         'status' => true,
+//         'message' => 'Response post method sensor ditemukan',
+//         'data' => null,
+//         'errors' => null,
+//     ];
+//     return response()->json($data);
+// });
+// Route::patch('sensor/{id}', function ($id) {
+//     $data = [
+//         'status' => true,
+//         'message' => 'Response patch method id ' . $id,
+//         'data' => null,
+//         'errors' => null,
+//     ];
+//     return response()->json($data);
+// });
+
+// Route::delete('sensor/{id}', function ($id) {
+//     $data = [
+//         'status' => true,
+//         'message' => 'Response delete method id ' . $id,
+//         'data' => null,
+//         'errors' => null,
+//     ];
+//     return response()->json($data);
+// });
